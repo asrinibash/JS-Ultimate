ddcff320d02014474d4116fbaf1195bb879333ce
A Srinibash Achary <126653603+asrinibash@users.noreply.github.com>
2023-08-28 20:46:33 +0530

    Conversion operations in JS

--- 01_Basics/04_ConversionOperation.js
+++ 01_Basics/04_ConversionOperation.js
@@ -27,9 +27,48 @@
 // let name="Srinu"; //-> true
 
 
-//3. String() -> convert to string
-let Score = 35
-let stringValue = String(Score); //-> convert to string
-console.log(typeof stringValue); //-> string
+// //3. String() -> convert to string
+// let Score = 35
+// let stringValue = String(Score); //-> convert to string
+// console.log(typeof stringValue); //-> string
 
 // Just like this we can convert to other datatypes also -->BigInt(), Symbol(), Object() etc
+
+
+
+
+// *************************Operators*************************
+let value=10;
+let negVlaue=-value; //-> -10
+
+// console.log(2+2); //-> Addition
+// console.log(2-2); //->Substraction
+// console.log(2*2); //->Multiplication
+// console.log(2**3); //->Exponentiation (2^3) -> 2 power 3 
+// console.log(2/2); //->Division
+// console.log(2%2); //->Modulus (Remainder)
+
+let str1="hello";     // Only Addition operator is used for string
+let str2=" Srinu";
+console.log(str1+str2); //-> Concatenation (hello Srinu)
+
+
+// Complex Operators  "Prefer to use brackets if needed other wise Dont write complex code"
+// console.log(1+"2"); //-> 12 
+// console.log("1"+2); //-> 12
+// console.log("1"+2+3); //-> 123 (1st string then number)
+// console.log(1+2+"3"); //-> 33  
+// console.log(1-"2"); //-> -1
+
+console.log(true); //-> true
+console.log(+true); //-> 1
+console.log(+""); //-> 0
+
+let num1,num2,num3;  //-> Declaring multiple variables in single line
+num1=num2=num3=10;   // But dont use like this because it is not readable
+
+
+// Increment and Decrement Operators
+let num=15;
+num++; //->post increment
+++num; //->pre increment
